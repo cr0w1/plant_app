19e8b8adbc49bf7bc1f274d70f3c2f919f3d88dd
cr0w1 <clebsonjose30@gmail.com>
2022-01-26 23:50:37 -0300

    home page

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/components/featured_plants.dart';
+import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
+import 'package:plant_app/screens/home/components/recomend_plants.dart';
+import 'package:plant_app/screens/home/components/title_with_more_btn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: 'Recomended', press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: 'Featured Plants', press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
