4dc45101f4718893f14dc810a6dd86cb86123a4d
cr0w1 <clebsonjose30@gmail.com>
2022-01-31 21:21:19 -0300

    implemented details screen

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,41 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/details/components/image_and_icons.dart';
+import 'package:plant_app/screens/details/components/title_and_price.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: Column(),
-            ),
-            Container(
-              height: size.height * 0.8,
-              width: size.width * 0.75,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 60,
-                      color: kPrimaryColor.withOpacity(0.29))
-                ],
-                image: DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/img.png'),
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIcons(size: size),
+          TitleAndPrice(
+            title: 'Angelica',
+            country: 'Russia',
+            price: 440,
+          ),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+                  color: kPrimaryColor,
+                  onPressed: () {},
+                  child: Text(
+                    'Buy Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text('Description'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
